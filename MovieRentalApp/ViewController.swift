ba06e8cee4b9159dd85768b9aa566d08995ba3d9
Alex Lawrence <alexnavis85@gmail.com>
2020-03-25 12:24:01 +0530

    Akash/Sarath | Set up repo | Create the IOS project with XCTest.

---
+++ MovieRentalApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MovieRentalApp
+//
+//  Created by Sarath Chenthamarai on 25/03/20.
+//  Copyright © 2020 Sarath Chenthamarai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
